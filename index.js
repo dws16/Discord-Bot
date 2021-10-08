cf0111311ec546d4c97eeb1565c2c7d828c53aa4
Firdaus Nanda Pradanggapasti <43716424+dws16@users.noreply.github.com>
2021-10-08 11:34:53 +0000

    repl.it

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Hello!!")
+})
+
+app.listen(3000, () => {
+  console.log("Bot is ready!")
+})
+
+
+
 const {
   Client,
   Intents
